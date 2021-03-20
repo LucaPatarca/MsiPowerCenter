a760330a3ccd7b9448cff4ee796f98ecb623fdc9
luca patarca <luca.patarca@gmail.com>
2021-03-21 00:43:44 +0100

    UI improvement and async task

--- gui/lib/main.dart
+++ gui/lib/main.dart
@@ -1,126 +1,135 @@
+import 'dart:ui';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'dart:io';
+import 'package:myapp/lib.dart';
+
+import 'package:myapp/profiles.dart';
+
+LibManager manager = new LibManager();
+Future<void> computeFuture = Future.value();
 
 void main() {
-  runApp(MyApp());
+  final manager = new LibManager();
+  ProfileStruct p = manager.readCurrentProfile();
+  print(p.getCpuMaxPerf());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MSI Power Center',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'MSI Power Center'),
+      home: HomePage(title: 'MSI Power Center'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  String _profile = "Battery";
+class _HomePageState extends State<HomePage> {
+  Profile _profile = Profile.Changing;
 
-  void _toggleProfile() {
+  VoidCallback createCallback(
+      BuildContext context, AsyncSnapshot snapshot, Profile profile) {
+    if (snapshot.connectionState == ConnectionState.done) {
+      return () {
+        setState(() {
+          computeFuture = computeOnSecondaryIsolate(profile)
+              .then((value) => {_profile = profile})
+              .catchError((e) {
+            ScaffoldMessenger.of(context)
+                .showSnackBar(SnackBar(content: Text("Unable to set profile")));
+          });
+        });
+      };
+    } else {
+      return null;
+    }
+  }
+
+  Future<void> computeOnSecondaryIsolate(Profile profile) async {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun 'the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      if (_profile == "Battery") {
-        _profile = "Normal";
-        Process.run('pkexec', [
-          "/home/luca/Scrivania/PowerCenter/cli/build/PowerCenter",
-          "normal"
-        ]).then((value) => print(value.stdout + value.stderr));
-      } else {
-        _profile = "Battery";
-        Process.run('pkexec', [
-          "/home/luca/Scrivania/PowerCenter/cli/build/PowerCenter",
-          "quiet"
-        ]).then((value) => print(value.stdout + value.stderr));
-      }
+      _profile = Profile.Changing;
     });
+    return await compute(fun, profile);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'The current profile is:',
-            ),
-            Text(
-              '$_profile',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            createProfileButton(Profile.Performance),
+            createProfileButton(Profile.Balanced),
+            createProfileButton(Profile.Silent),
+            createProfileButton(Profile.Battery)
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _toggleProfile,
-        tooltip: 'Toggle profile',
-        child: Icon(Icons.edit),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  FutureBuilder createProfileButton(Profile profile) {
+    return FutureBuilder(
+      future: computeFuture,
+      builder: (context, snapshot) {
+        return TextButton(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(
+                  getIconData(profile),
+                  color: _profile == profile
+                      ? Theme.of(context).accentColor
+                      : Theme.of(context).unselectedWidgetColor,
+                  size: 120,
+                ),
+                Text(
+                  profile.toString().replaceFirst("Profile.", ""),
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+              ],
+            ),
+            onPressed: createCallback(context, snapshot, profile));
+      },
+    );
+  }
+
+  IconData getIconData(Profile profile) {
+    switch (profile) {
+      case Profile.Performance:
+        return Icons.speed;
+      case Profile.Balanced:
+        return Icons.equalizer;
+      case Profile.Silent:
+        return Icons.hearing_disabled;
+      case Profile.Battery:
+        return Icons.battery_full;
+      default:
+        return null;
+    }
+  }
+}
+
+void fun(Profile profile) {
+  manager.writeProfile(profile);
 }
